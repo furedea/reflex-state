9de9f9fe5bead3b7fe93b254fa65df42d81b7d7b
Kaito Shigyo / 執行凱斗 <132188853+furedea@users.noreply.github.com>
2026-09-17 15:09:52 +0900

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-25.11-darwin";
+  };
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      system = "aarch64-darwin";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          commitlint
+          deadnix
+          lefthook
+          ls-lint
+          nixfmt-rfc-style
+          nodejs_24
+          pnpm
+          statix
+        ];
+      };
+    };
+}
